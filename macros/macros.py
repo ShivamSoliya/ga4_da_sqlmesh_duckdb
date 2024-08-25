4f95ee650440233fc84c3a1f22799de070f0c8a6
ShivamSoliya <shivamsoliya10100@protonmail.com>
2024-08-25 13:33:46 +0530

    modified model and macro codes

--- macros/macros.py
+++ macros/macros.py
@@ -1,18 +1,19 @@
 from sqlmesh import macro
-import datetime
+from datetime import datetime, date, timedelta
+import pytz
 
 @macro()
 def get_date_range_list(evaluator):
-    start_date = datetime.date(2021, 1, 31)
-    end_date = datetime.date(2021, 1, 31)
+    start_date = date(2021, 1, 31)
+    end_date = date(2021, 1, 31)
     
     return [[f"/mnt/{date}/*" 
-             for date in [(start_date + datetime.timedelta(days=i)).strftime('%Y%m%d') 
+             for date in [(start_date + timedelta(days=i)).strftime('%Y%m%d') 
                 for i in range((end_date - start_date).days + 1)]]]
 
 @macro()
 def get_output_uri(evaluator):
-    return f"/mnt/output/{int(datetime.utcnow().timestamp())}/latest_session.csv"
+    return f"/mnt/output/{datetime.now(pytz.timezone('Asia/Kolkata')).strftime("%Y%m%d/%H%M%S")}/latest_session.csv"
                 
 @macro()
 def get_param_value(evaluator, param, target_key: str):
